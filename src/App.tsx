c98c17a2d30070ba98688d61acc02869517736bf
PatricSPires <patric.pires@outlook.com>
2020-06-15 16:31:26 -0300

    feat(header): create switch and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
 
 function App() {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
   return (
-    <ThemeProvider theme={light}>
+    <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
           <GlobalStyle />
-          <Header />
+          <Header toggleTheme={toggleTheme} />
         </header>
       </div>
     </ThemeProvider>
